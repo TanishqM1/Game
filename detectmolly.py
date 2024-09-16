6a9bff3a63813b686749f6683dceb8519d2642a0
Tanishq Mehta <tanishqmehta2004@gmail.com>
2024-09-15 19:24:41 -0700

    added burn folder

--- detectmolly.py
+++ detectmolly.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import tensorflow as tf 
 import tensorflow_io as tfio
 
-MOLLY_FILE = os.path.join('data', 'Parsed_Capuchinbird_Clips', 'molotov.wav')
+MOLLY_FILE = os.path.join('data', 'Molotov_Clips', 'molotov_sound.wav')
 NOT_MOLLY_FILE = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips', 'notmolotov.wav')
 
 
